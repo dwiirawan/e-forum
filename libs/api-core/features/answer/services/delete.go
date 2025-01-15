53fde1ea08155ce3f9117ba20f1cf43744d3620b
alvinadet <alvinadetyaworks@gmail.com>
2025-01-15 20:35:33 +0700

    add delete method on answer

--- libs/api-core/features/answer/services/delete.go
+++ libs/api-core/features/answer/services/delete.go
@@ -8,7 +8,9 @@ import (
 )
 
 func (s *AnswerService) Delete(id string) error {
-	if err := s.db.Delete(&models.AnswerModel{}, id).Error; err != nil {
+	if err := s.db.Delete(&models.AnswerModel{
+		ID: id,
+	}).Error; err != nil {
 		return utils.NewError(fiber.StatusInternalServerError, "E_DELETE_ANSWER", "failed to delete answer", err)
 	}
 	return nil
